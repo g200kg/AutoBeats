28419f543c3447317b9c0e5bf265954724758757
g200kg <gaito@g200kg.com>
2014-11-12 15:49:52 +0900

    improve percussion

--- autobeats.js
+++ autobeats.js
@@ -1,8 +1,10 @@
 function AutoBeats(title,bpm,audioctx,dest){
-	function FMO(audioctx,dest,params) {
+	function FMO(audioctx,dest,send,params) {
 		this.v=[]; this.r=[]; this.Op=[]; this.Gain=[];
+		this.h=0;
 		this.Pan=audioctx.createPanner();
 		this.Pan.panningModel="equalpower";
+		this.Send=audioctx.createGain();
 		for(var i=0;i<3;++i){
 			this.v[i]=params[i*4+1];
 			this.r[i]=params[i*4+2];
@@ -20,13 +22,21 @@ function AutoBeats(title,bpm,audioctx,dest){
 		var th=Math.PI*(params[12]-.5);
 		this.Pan.setPosition(Math.sin(th),0,-Math.cos(th));
 		this.Pan.connect(dest);
+		this.Send.gain.value=0.2;
+		this.Pan.connect(this.Send);
+		this.Send.connect(send);
 		this.trig=function(t) {
 			for(var i=0;i<3;++i){
-				this.Gain[i].gain.setValueAtTime(0,t-0.01);
-				this.Gain[i].gain.linearRampToValueAtTime(this.v[i],t);
-				this.Gain[i].gain.setTargetAtTime(0,t+0.01,this.r[i]);
+				this.Gain[i].gain.cancelScheduledValues(t);
+				this.Gain[i].gain.setValueAtTime(0,t);
+				this.Gain[i].gain.linearRampToValueAtTime(this.v[i],t+0.005);
+				this.Gain[i].gain.linearRampToValueAtTime(0,t+0.006+this.h*0.1);
 			}
 		};
+		this.setTone=function(f,h){
+			this.Op[0].detune.value=f*24;
+			this.h=h*0.01;
+		}
 	};
 	function OSC(audioctx,dest,send,params){
 		this.Osc=audioctx.createOscillator();
@@ -52,12 +62,11 @@ function AutoBeats(title,bpm,audioctx,dest){
 		var th=Math.PI*(params.pan-.5);
 		this.Pan.setPosition(Math.sin(th),0,-Math.cos(th));
 		this.note=function(n,v,t){
-			this.Gain.gain.setValueAtTime(0,t-0.01);
-			this.Gain.gain.linearRampToValueAtTime(v*this.vol,t);
-			this.Gain.gain.setTargetAtTime(0,t,this.sus);
 			var f=440*Math.pow(2,(n-69+this.oct*12)/12);
 			this.Osc.frequency.setValueAtTime(f,t);
 			this.Filter.frequency.setValueAtTime(f*this.cutoff,t);
+			this.Gain.gain.setValueAtTime(v*this.vol,t);
+			this.Gain.gain.setTargetAtTime(0,t+0.01,this.sus);
 		};
 		this.setTone=function(type,cutoff,q,sus){
 			this.Osc.type=["sine","sawtooth","square","square"][type];
@@ -65,23 +74,30 @@ function AutoBeats(title,bpm,audioctx,dest){
 			this.Filter.Q.value=q;
 			this.sus=sus;
 		};
+		this.mute=function(){
+			this.Gain.gain.cancelScheduledValues(0);
+			this.Gain.gain.value=0;
+		}
 	};
 	this.getSeq=function(k,i){
 		return Math.abs(Math.sin(this.h[0]*k*i)*100)|0;
 	};
-	this.p0="54645466546654661020142454055466";
+	this.p0="5464546654665466102014245405546640445466546654661020142454057777";
 	this.s0=[
 		[0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15],
 		[0, 2, 4, 5, 7, 9,11,12,14,16,17,19,21,23,24,26],
 		[0, 2, 4, 6, 8,10,12,14,16,18,20,22,24,26,28,30],
 		[0, 2, 3, 5, 6, 8, 9,11,12,14,15,17,18,20,21,23]
 	];
-	this.b0=[90,110,120,140,160,180,220,240];
+	this.b0=[90,100,120,140,160,180,200,220,240];
 	this.start=function(){
 		this.run=1;
 	};
 	this.stop=function(){
 		this.run=0;
+		this.o1.mute();
+		this.o2.mute();
+		this.o3.mute();
 	};
 	this.getBpm=function(){
 		return this.bpm;
@@ -108,10 +124,14 @@ function AutoBeats(title,bpm,audioctx,dest){
 			this.h[2][i]=(Math.abs(Math.sin(this.h[0]*2222*i)*100))|0;
 			this.h[3][i]=(Math.abs(Math.sin(this.h[0]*1234*i)*100))|0;
 		}
+		this.bdo.setTone(this.getSeq(10,1),this.getSeq(11,10));
+		this.sdo.setTone(this.getSeq(12,1),this.getSeq(13,10));
+		this.hho.setTone(this.getSeq(14,1),this.getSeq(15,10));
 		this.scale=this.s0[this.getSeq(4,1)&3];
 		this.setBpm(this.b0[this.getSeq(this.h[1],4)&7]);
 	};
 	if(!audioctx){
+		AudioContext=window.AudioContext||window.webkitAudioContext;
 		audioctx=new AudioContext();
 		dest=audioctx.destination;
 	}
@@ -122,12 +142,16 @@ function AutoBeats(title,bpm,audioctx,dest){
 	this.meas=0;
 	this.beat=0;
 	this.Comp=audioctx.createDynamicsCompressor();
-	this.scrproc=audioctx.createScriptProcessor(1024,2,2);
+	this.scrproc=audioctx.createScriptProcessor(2048,1,1);
 	this.scrproc.ab=this;
+	this.scrproc.connect(this.Comp);
+	this.dummyosc=audioctx.createOscillator();
+	this.dummyosc.connect(this.scrproc);
+	this.dummyosc.start(0);
 	this.delay=audioctx.createDelay();
 	this.delayFb=audioctx.createGain();
 	this.delayFb.gain.value=0.5;
-	this.delay.delayTime=30/this.bpm;
+	this.delay.delayTime.value=30/this.bpm;
 	this.delay.connect(this.Comp);
 	this.delay.connect(this.delayFb);
 	this.delayFb.connect(this.delay);
@@ -135,9 +159,6 @@ function AutoBeats(title,bpm,audioctx,dest){
 	this.scrproc.onaudioprocess=function(e){
 		var ab=this.ab;
 		if(!ab.run){
-			ab.o1.note(0,0,ab.cur);
-			ab.o2.note(0,0,ab.cur);
-			ab.o3.note(0,0,ab.cur);
 			ab.meas=ab.beat=0;
 			ab.cur=ab.audioctx.currentTime;
 			return;
@@ -146,13 +167,10 @@ function AutoBeats(title,bpm,audioctx,dest){
 		var t=ab.audioctx.currentTime;
 		while(t>=ab.cur+st-0.1){
 			ab.cur+=st;
-			var pb=ab.getSeq(1,ab.meas)&3;
-			var ps=ab.getSeq(2,ab.meas)&3;
-			var ph=ab.getSeq(3,ab.meas)&3;
 			var pn=ab.beat&7;
-			if(ab.p0[pb*8+pn]&1) ab.bdo.trig(ab.cur);
-			if(ab.p0[ps*8+pn]&2) ab.sdo.trig(ab.cur);
-			if(ab.p0[ph*8+pn]&4) ab.hho.trig(ab.cur);
+			if(ab.p0[(ab.getSeq(1,ab.meas)&7)*8+pn]&1) ab.bdo.trig(ab.cur);
+			if(ab.p0[(ab.getSeq(2,ab.meas)&7)*8+pn]&2) ab.sdo.trig(ab.cur);
+			if(ab.p0[(ab.getSeq(3,ab.meas)&7)*8+pn]&4) ab.hho.trig(ab.cur);
 			var r1=ab.getSeq(2,ab.meas*2+ab.beat);
 			var r2=ab.getSeq(3,ab.meas*2+ab.beat);
 			var r3=ab.getSeq(4,ab.meas*2+ab.beat);
@@ -161,17 +179,17 @@ function AutoBeats(title,bpm,audioctx,dest){
 			ab.o3.note(ab.scale[(r3&15)]+57,r3&1,ab.cur);
 			if(++ab.beat>=16){
 				ab.beat=0;
-				++ab.meas;
+				if(++ab.meas>=64)
+					ab.meas=0;
 				ab.o1.setTone(ab.getSeq(4,ab.meas)&3,(ab.getSeq(5,ab.meas)&15)+4,(ab.getSeq(6,ab.meas)&15)+5,Math.pow(0.9,ab.getSeq(13,ab.meas)&31));
 				ab.o2.setTone(ab.getSeq(7,ab.meas)&3,(ab.getSeq(8,ab.meas)&15)+4,(ab.getSeq(9,ab.meas)&15)+5,Math.pow(0.9,ab.getSeq(14,ab.meas)&31));
 				ab.o3.setTone(ab.getSeq(10,ab.meas)&3,(ab.getSeq(11,ab.meas)&15)+4,(ab.getSeq(12,ab.meas)&15)+5,Math.pow(0.9,ab.getSeq(15,ab.meas)&31));
 			}
 		}
 	};
-	this.scrproc.connect(this.Comp);
-	this.bdo=new FMO(audioctx,this.Comp,[25,1.,.05,2, 29,400,.01,0, 131,100,.001,0,0.5]);
-	this.sdo=new FMO(audioctx,this.Comp,[213,.6,.1,0, 723,1900,.6,0, 140,800,.1,0,0.4]);
-	this.hho=new FMO(audioctx,this.Comp,[2200,.3,.02,0, 423,3120,.5,0, 552,1800,.1,1,.7,0.6]);
+	this.bdo=new FMO(audioctx,this.Comp,this.delay,[25,1.,.05,2, 29,400,.01,0, 131,100,.001,0,0.5]);
+	this.sdo=new FMO(audioctx,this.Comp,this.delay,[213,.6,.1,0, 123,1900,.6,0, 340,800,.1,0,0.4]);
+	this.hho=new FMO(audioctx,this.Comp,this.delay,[2200,.3,.02,0, 423,3120,.5,0, 552,1800,.1,1,.7,0.6]);
 	this.o1=new OSC(audioctx,this.Comp,this.delay,{'oct':-2,'vol':0.5,'pan':0.5,'sus':.1});
 	this.o2=new OSC(audioctx,this.Comp,this.delay,{'oct':0,'vol':0.3,'pan':0.35,'sus':.1});
 	this.o3=new OSC(audioctx,this.Comp,this.delay,{'oct':1,'vol':0.3,'pan':0.65,'sus':.1});
